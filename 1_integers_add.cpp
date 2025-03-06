c88c1ee385a7d1a6b4e5fe4c026dc0c7191ee5a5
yhliu <2658686623@qq.com>
2025-03-06 14:20:34 +0800

    +1

---
+++ 1_integers_add.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x, y;
+	cout << "Enter two integers: ";
+	cin >> x >> y;
+	int c = x + y;
+	cout << "x + y =  " << c << endl;
+	return 0;
+
+}
